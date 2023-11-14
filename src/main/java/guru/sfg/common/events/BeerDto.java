65a2bd68e4e98fa9faf6d77ee600408afcd410db
Andre S Nascimento <andresoaresnascimento@gmail.com>
2023-11-14 17:26:24 -0300

    Após aula 198 - Final da Seção 14 JMS

---
+++ src/main/java/guru/sfg/common/events/BeerDto.java
@@ -0,0 +1,58 @@
+package guru.sfg.common.events;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+  @Serial private static final long serialVersionUID = 5706374617046143969L;
+
+  @Null
+  private UUID id;
+
+  @Null
+  private Integer version;
+
+  @Null
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:mm:ss.SSSSSSXXXXX", shape = Shape.STRING)
+  private OffsetDateTime createdDate;
+
+  @Null
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:mm:ss.SSSSSSXXXXX", shape = Shape.STRING)
+  private OffsetDateTime lastModifiedDate;
+
+  @NotBlank
+  private String beerName;
+
+  @NotNull
+  @Valid
+  private String beerStyle;
+
+  @NotNull
+  private String upc;
+
+  @NotNull
+  @Positive
+  @JsonFormat(shape = Shape.STRING)
+  private BigDecimal price;
+
+  private Integer quantityOnHand;
+}
---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,22 @@
+package guru.sfg.common.events;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+  @Serial private static final long serialVersionUID = -1321L;
+
+  private BeerDto beerDto;
+
+  public BeerEvent() {
+    super();
+  }
+}
